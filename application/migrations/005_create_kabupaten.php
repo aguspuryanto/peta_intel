c0dc774c3f309312396bd84d34084055b30777df
Agus Puryanto <aguspuryanto@gmail.com>
2023-09-15 06:10:34 +0700

    update

---
+++ application/migrations/005_create_kabupaten.php
@@ -0,0 +1,37 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); 
+
+class Migration_create_meeting extends CI_Migration {
+    private $tableName = 'epak_kabupaten';
+    public function up() { 
+            $this->dbforge->add_field(array(
+            'id' => array(
+                'type' => 'INT',
+                'constraint' => 20,
+                'unsigned' => TRUE,
+                'auto_increment' => TRUE
+            ),
+            'kode' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '50'
+            ),
+            'nama' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '150'
+            ),
+            'prov_id' => array(
+                'type' => 'INT',
+                'constraint' => '11'
+            ),
+            'created_at datetime default current_timestamp'
+        ));
+        $this->dbforge->add_key('id', TRUE);
+        if (!$this->db->table_exists($this->tableName)) {
+            $this->dbforge->create_table($this->tableName, FALSE, array('ENGINE' => 'InnoDB'));
+        }
+    }
+
+    public function down()
+    {
+        $this->dbforge->drop_table($this->tableName);
+    }
+}
