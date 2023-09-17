d08fd0cddb97bda71128a9f545baeeeef0915cc2
Agus Puryanto <aguspuryanto@gmail.com>
2023-09-17 11:12:30 +0700

    update

---
+++ application/migrations/011_create_penyuluhan.php
@@ -0,0 +1,39 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); 
+
+class Migration_create_penyuluhan extends CI_Migration {
+    private $tableName = 'epak_penyuluhan';
+    public function up() { 
+            $this->dbforge->add_field(array(
+            'id' => array(
+                'type' => 'INT',
+                'constraint' => 20,
+                'unsigned' => TRUE,
+                'auto_increment' => TRUE
+            ),
+            'kecamatan' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '150'
+            ),
+            'jenis' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '150',
+                'null' => TRUE
+            ),
+            'lokasi' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '150',
+                'null' => TRUE
+            ),
+            'created_at datetime default current_timestamp'
+        ));
+        $this->dbforge->add_key('id', TRUE);
+        if (!$this->db->table_exists($this->tableName)) {
+            $this->dbforge->create_table($this->tableName, FALSE);
+        }
+    }
+
+    public function down()
+    {
+        $this->dbforge->drop_table($this->tableName);
+    }
+}
