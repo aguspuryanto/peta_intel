53065f334f3d48c867835eb31d60428ca56783ba
Agus Puryanto <aguspuryanto@gmail.com>
2023-09-17 12:14:37 +0700

    update

---
+++ application/controllers/Polsosbud.php
@@ -0,0 +1,105 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Polsosbud extends CI_Controller {
+
+	public function __construct()
+    {
+        parent::__construct();
+        is_logged_in();
+
+		$this->load->model('M_provinsi');
+		$this->load->model('M_kabupaten');
+		$this->load->model('M_kecamatan');
+		$this->load->model('M_polsusbud');
+    }
+
+	public function index()
+	{
+		$data['title'] = "Politik, Sosial Budaya dan Sumber Daya Organisasi";
+		$data['konten'] = "index";
+
+		$data['listKab'] = array();		
+		$listKab = $this->M_kecamatan->select_all();
+		foreach($listKab as $kab) {
+			$data['listKab'][$kab->id] = $kab->nama;
+		}
+
+		$data['model'] = $this->M_polsusbud;
+		$data['dataProvider'] = $this->M_polsusbud->select_all();
+		
+		// $this->load->view('template/layout', $data);
+		$this->template->views('page/polsusbud/index', $data);
+	}
+
+	public function create() {
+		
+		$this->load->library('form_validation');
+		
+		$model = $this->M_polsusbud;
+
+        $json = array();
+		$this->form_validation->set_rules($model->rules());
+		$this->form_validation->set_message('required', 'Mohon lengkapi {field}!');
+
+		if (!$this->form_validation->run()) {			
+			foreach($model->rules() as $key => $val) {
+				$json = array_merge($json, array(
+					$val['field'] => form_error($val['field'], '<p class="mt-3 text-danger">', '</p>')
+				));
+			}
+		} else {
+			$data = array(
+				'kecamatan' => $this->input->post('kecamatan'),
+				'jenis' => $this->input->post('jenis'),
+				'lokasi' => $this->input->post('lokasi')
+			);
+
+			if($this->input->post('id')) {
+				$id = $this->input->post('id');
+				$model->update($id, $data);				
+			} else {
+				$model->save($data);
+			}
+			
+            $this->session->set_flashdata('success', 'Berhasil disimpan');
+			$json = array('success' => true, 'message' => 'Berhasil disimpan');
+		}
+
+		$this->output
+        ->set_content_type('application/json')
+        ->set_output(json_encode($json));
+	}
+
+	public function view($id) {
+		$data['data'] = $this->M_polsusbud->selectId($id);
+
+		$json = array();
+		if($data['data']) {
+			$json = array('success' => true, 'data' => $data['data']);
+		} else {
+			$json = array('success' => false, 'data' => []);
+		}
+
+		$this->output
+        ->set_content_type('application/json')
+        ->set_output(json_encode($json));
+	}
+
+	public function remove() {		
+		$json = array();
+		$model = $this->M_polsusbud;
+
+		if($this->input->post('id')) {
+			$id = $this->input->post('id');
+			$model->delete($id);
+
+			$this->session->set_flashdata('success', 'Berhasil terhapus');
+			$json = array('success' => true, 'message' => 'Berhasil terhapus');
+		}
+
+		$this->output
+		->set_content_type('application/json')
+		->set_output(json_encode($json));
+	}
+}
