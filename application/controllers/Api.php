22012d905454aeda60fcf21ce72c0d39e81abe49
Agus Puryanto <aguspuryanto@gmail.com>
2023-09-19 05:35:38 +0700

    update

---
+++ application/controllers/Api.php
@@ -0,0 +1,100 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Api extends CI_Controller {
+
+	public function __construct()
+    {
+        parent::__construct();
+        // is_logged_in();
+		
+		$this->load->model('M_kabupaten');
+		$this->load->model('M_kecamatan');
+    }
+
+	public function index()
+	{
+		// $data['title'] = "PETA INTELIJEN & BANK DATA INTEL";
+		// $data['konten'] = "index";
+		
+		// $this->load->view('template/layout', $data);
+		// $this->template->views('page/home', $data);
+	}
+
+	public function penyelamatan_keuangan() {
+		$data['title'] = "PENYELAMATAN KEUANGAN NEGARA DAN PENANGGULANGAN TINDAK PIDANA";
+
+		$data['listLatLong'] = array();		
+		$data['listKab'] = array();		
+		$listKab = $this->M_kabupaten->select_all();
+		foreach($listKab as $kab) {			
+			$data['listKab'][] = array(
+				'type' => 'Feature',
+				'properties' => array(
+					'name' => $kab->nama,
+					'amenity' => $kab->nama,
+					'popupContent' => $kab->nama,
+					'show_on_map' => true
+				),
+				'geometry' => array(
+					'type' => 'Point',
+					'coordinates' => array($kab->latitude, $kab->longitude)
+				),
+			);
+
+			$data['listLatLong'] = array(
+				'name' => $kab->nama,
+				'coordinates' => array($kab->latitude, $kab->longitude)
+			);
+		}
+		
+		$data['listGeoJson'][] = array(
+			'type' => 'FeatureCollection',
+			'features' => $data['listKab']
+		);
+
+		$this->load->view('page/api/pkn', $data);
+	}
+
+	public function penyuluhan_hukum() {
+		$data['title'] = "PENYULUHAN DAN PENERANGAN HUKUM";
+
+		$this->load->view('page/api/penyuluhan', $data);
+	}
+	
+	public function politik() {
+		$data['title'] = "PENYULUHAN DAN PENERANGAN HUKUM";
+
+		$this->load->view('page/api/penyuluhan', $data);
+	}
+
+	public function pemilu_1() {
+		$data['title'] = "PENYULUHAN DAN PENERANGAN HUKUM";
+
+		$this->load->view('page/api/penyuluhan', $data);
+	}
+
+	public function pemilu_2() {
+		$data['title'] = "PENYULUHAN DAN PENERANGAN HUKUM";
+
+		$this->load->view('page/api/penyuluhan', $data);
+	}
+
+	public function pemilu_3() {
+		$data['title'] = "PENYULUHAN DAN PENERANGAN HUKUM";
+
+		$this->load->view('page/api/penyuluhan', $data);
+	}
+
+	public function penyelamatan_keuangan_k() {
+		$data['title'] = "PENYULUHAN DAN PENERANGAN HUKUM";
+
+		$this->load->view('page/api/penyuluhan', $data);
+	}
+
+	public function penyuluhan_hukum_k() {
+		$data['title'] = "PENYULUHAN DAN PENERANGAN HUKUM";
+
+		$this->load->view('page/api/penyuluhan', $data);
+	}
+}
---
+++ application/views/page/api/penyuluhan.php
@@ -0,0 +1,100 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+
+  <title>KEJARI BANDAR LAMPUNG</title>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin=""/>
+    <script src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js" integrity="sha512-GffPMF3RvMeYyc1LWMHtK8EbPv0iNZ8/oTtHPx9/cc2ILxQ+u905qIwdpULaqDkyBKgOaB57QTMg7ztg8Jm2Og==" crossorigin=""></script>
+  <style>
+    html, body {
+      height: 100%;
+      margin: 0;
+    }
+    #map {
+      width: 100%;
+      height:  100%;
+    }
+  .info { padding: 6px 8px; font: 14px/16px Arial, Helvetica, sans-serif; background: white; background: rgba(255,255,255,0.8); box-shadow: 0 0 15px rgba(0,0,0,0.2); border-radius: 5px; } .info h4 { margin: 0 0 5px; color: #777; }
+  .legend { text-align: left; line-height: 18px; color: #555; }
+  .legend i { width: 18px; height: 18px; float: left; margin-right: 8px; opacity: 0.7; }
+
+  .myButton {
+	background-color:#44c767;
+	-moz-border-radius:28px;
+	-webkit-border-radius:28px;
+	border-radius:28px;
+	border:1px solid #18ab29;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Arial;
+	font-size:17px;
+	padding:16px 31px;
+	text-decoration:none;
+	text-shadow:0px 1px 0px #2f6627;
+}
+.myButton:hover {
+	background-color:#5cbf2a;
+}
+.myButton:active {
+	position:relative;
+	top:1px;
+}
+
+
+
+</style>
+</head>
+    <body>
+    <div id='map'>
+      <div class="leaflet-top leaflet-right">
+      </div>
+    </div>
+    </body>
+
+    <script type="text/javascript" src="<?=base_url('assets/');?>js/icon.js"></script>
+    <script type="text/javascript" src="<?=base_url('assets/');?>js/kecamatan.js"></script>
+
+    <script type="text/javascript">
+
+        var map = L.map('map').setView([-5.40855,105.25932], 12);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          maxZoom: 19,
+          attribution: 'Kejari Bandar Lampung'
+        }).addTo(map);
+
+        function style(feature) {
+          return {
+            weight: 1,
+            opacity: 1,
+            color: 'white',
+            dashArray: '3',
+            fillOpacity: 0.5,
+            fillColor: "green",
+            border: 3
+          };
+        }
+
+        var geojson;
+        geojson = L.geoJson(datakecamatan, {
+          style: style
+        }).addTo(map);
+
+        
+        var legend = L.control({position: 'bottomleft'});
+        legend.onAdd = function (map) {
+          var div = L.DomUtil.create('div', 'info legend'),
+            grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+            labels = [],
+            from, to;
+
+            labels.push('<img src="<?=base_url('assets/');?>icon/legend_penyelamatan_keuangan_negara.jpg">');
+            div.innerHTML = labels.join('<br>');
+            return div;
+        };
+
+      legend.addTo(map);
+    </script>
+  </html>
