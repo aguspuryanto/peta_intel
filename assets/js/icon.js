6b7ea2e155b955e789c71fc1e82e1f9bee2329e9
Agus Puryanto <aguspuryanto@gmail.com>
2023-09-19 05:40:24 +0700

    update

---
+++ assets/js/icon.js
@@ -0,0 +1,65 @@
+
+//Penkum
+var Tindak_Pidana_Korupsi = L.icon({
+    iconUrl: 'icon/Tindak_Pidana_Korupsi.png',
+    iconSize:     [35, 35], // size of the icon
+})
+var Tindak_Pidana_Cukai = L.icon({
+    iconUrl: 'icon/Tindak_Pidana_Cukai.png',
+    iconSize:     [35, 35], // size of the icon
+})
+var Datun = L.icon({
+    iconUrl: 'icon/datun.png',
+    iconSize:     [35, 35], // size of the icon
+})
+var kosong = L.icon({
+    iconUrl: 'icon/cukai.png',
+    iconSize:     [1, 1], // size of the icon
+
+})
+
+//Politik
+var Keimigrasian = L.icon({
+    iconUrl: 'icon/politik/Keimigrasian.png',
+    iconSize:     [35, 35], // size of the icon
+})
+var Narkoba = L.icon({
+    iconUrl: 'icon/politik/Narkoba.png',
+    iconSize:     [35, 35], // size of the icon
+})
+var Dakwah_Negatif = L.icon({
+    iconUrl: 'icon/politik/Dakwah Negatif.png',
+    iconSize:     [35, 35], // size of the icon
+    popupAnchor:  [-5, -5],
+})
+var Teror = L.icon({
+    iconUrl: 'icon/politik/Teror.png',
+    iconSize:     [35, 35], // size of the icon
+    popupAnchor:  [-5, -5],
+})
+var Unjuk_Rasa = L.icon({
+    iconUrl: 'icon/politik/Unjuk Rasa.png',
+    iconSize:     [40, 40], // size of the icon
+    popupAnchor:  [-5, -5],
+})
+
+var belum = L.icon({
+    iconUrl: 'icon/belum.png',
+    popupAnchor:  [-5, -5],
+    iconSize:     [35, 35], // size of the icon
+})
+var sudah = L.icon({
+    popupAnchor:  [-5, -5],
+    iconUrl: 'icon/sudah.png',
+    iconSize:     [35, 35], // size of the icon
+})
+var akan = L.icon({
+    popupAnchor:  [-5, -5],
+    iconUrl: 'icon/akan.png',
+    iconSize:     [35, 35], // size of the icon
+})
+var telah = L.icon({
+    popupAnchor:  [-5, -5],
+    iconUrl: 'icon/telah.png',
+    iconSize:     [35, 35], // size of the icon
+})
---
+++ assets/js/nama_daerah.js
@@ -0,0 +1,24 @@
+
+//Rajabasa
+L.marker(['-5.3525','105.24456'],{icon: kosong}).addTo(map).bindPopup("Rajabasa", {autoClose:false} ).openPopup();
+
+//Labuhan Ratu
+L.marker(['-5.35899','105.27821'],{icon: kosong}).addTo(map).bindPopup("Labuhan Ratu", {autoClose:false} ).openPopup();
+
+//Tanjung Karang barat
+L.marker(['-5.41915','105.22945'],{icon: kosong}).addTo(map).bindPopup("Tj. karang barat", {autoClose:false} ).openPopup();
+
+ //Kemiling
+ L.marker(['-5.40308 ','105.191'],{icon: kosong}).addTo(map).bindPopup("Kemiling", {autoClose:false} ).openPopup();
+
+ //Teluk Betung barat
+ L.marker([' -5.48203','105.2401'],{icon: kosong}).addTo(map).bindPopup("Teluk Btg barat", {autoClose:false} ).openPopup();
+
+ //Panjang
+ L.marker(['-5.43863 ','105.31803'],{icon: kosong}).addTo(map).bindPopup("Panjang", {autoClose:false} ).openPopup();
+
+ //Sukabumi
+ L.marker(['-5.40172 ','105.28198'],{icon: kosong}).addTo(map).bindPopup("Sukabumi", {autoClose:false} ).openPopup();
+
+ //Teluk Betung Selatan
+ L.marker(['-5.43316','105.26962'],{icon: kosong}).addTo(map).bindPopup("Teluk Btg  Selatan", {autoClose:false} ).openPopup();
