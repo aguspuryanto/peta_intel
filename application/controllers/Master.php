312ac848dc8b21eb197967e6a3a325c19bcbf9e6
Agus Puryanto <aguspuryanto@gmail.com>
2023-09-27 05:46:11 +0700

    update

--- application/controllers/Master.php
+++ application/controllers/Master.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Pilpres extends CI_Controller {
+class Master extends CI_Controller {
 
 	public function __construct()
     {
