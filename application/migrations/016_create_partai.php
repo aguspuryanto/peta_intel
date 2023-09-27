c8a9fcb778a4cbbaf2f9cd1c2a486a5579e1bf4b
Agus Puryanto <aguspuryanto@gmail.com>
2023-09-28 05:19:43 +0700

    update

---
+++ application/migrations/016_create_partai.php
@@ -0,0 +1,33 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); 
+
+class Migration_create_partai extends CI_Migration {
+    private $tableName = 'epak_partai';
+    public function up() { 
+            $this->dbforge->add_field(array(
+            'id' => array(
+                'type' => 'INT',
+                'constraint' => 20,
+                'unsigned' => TRUE,
+                'auto_increment' => TRUE
+            ),
+            'thn' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '4'
+            ),
+            'nama_partai' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '150'
+            ),
+            'created_at' => array('type' => 'timestamp')
+        ));
+        $this->dbforge->add_key('id', TRUE);
+        if (!$this->db->table_exists($this->tableName)) {
+            $this->dbforge->create_table($this->tableName, FALSE);
+        }
+    }
+
+    public function down()
+    {
+        $this->dbforge->drop_table($this->tableName);
+    }
+}
