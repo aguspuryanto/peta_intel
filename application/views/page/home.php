a31f3ca62c3cedc8a0832e653ee461993a3870c4
Agus Puryanto <aguspuryanto@gmail.com>
2023-10-02 18:49:35 +0700

    update

--- application/views/page/home.php
+++ application/views/page/home.php
@@ -26,23 +26,13 @@
     <nav class="navbar navbar-expand-lg navbar-success bg-success text-white justify-content-between">
         <div class="container">
             <img src="<?=base_url('assets/');?>img/favicon.png" alt="logo Kejaksaan" class="mr-3">
-            <a class="navbar-brand text-white" href="#"><?=@$title; ?></a>
+            <a class="navbar-brand text-white" href="<?=site_url('index') ?>"><?=@$title; ?></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
                 aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse mr-auto" id="navbarNavAltMarkup">
             <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-                <!-- <li class="nav-item dropdown active">
-                    <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Bank Data</a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li> -->
-
                 <?php foreach($bankData as $bank) {
                     echo '<li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle text-white" href="'. base_url($bank['url']) . '" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">' . $bank['title'] . '</a>
