311fab3fe70272087aac20c22c09f72adf104103
Agus Puryanto <aguspuryanto@gmail.com>
2023-10-02 12:13:11 +0700

    update

--- application/models/M_anggota_dprd.php
+++ application/models/M_anggota_dprd.php
@@ -7,10 +7,11 @@ class M_anggota_dprd extends CI_Model {
     public function rules()
     {
         return [
-            ['field' => 'periode', 'label' => 'Periode', 'rules' => ''],
-            ['field' => 'partai_id', 'label' => 'Nama Partai'],
-            ['field' => 'dapil_id', 'label' => 'Nama Dapil', 'rules' => ''],
-            ['field' => 'jml_suara', 'label' => 'Jumlah Suara', 'rules' => ''],
+            ['field' => 'periode', 'label' => 'Periode', 'rules' => 'required'],
+            ['field' => 'nama_anggota', 'label' => 'Nama Anggota', 'rules' => 'required'],
+            ['field' => 'partai_id', 'label' => 'Nama Partai', 'rules' => 'required'],
+            ['field' => 'dapil_id', 'label' => 'Nama Dapil', 'rules' => 'required'],
+            ['field' => 'jml_suara', 'label' => 'Jumlah Suara', 'rules' => 'required'],
             ['field' => 'keterangan', 'label' => 'Anggota', 'rules' => ''],
         ];
     }    
