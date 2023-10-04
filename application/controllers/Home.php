f665f5a0f397f5dcb6d7c5fa4503bd8c524bad0d
Agus Puryanto <aguspuryanto@gmail.com>
2023-10-04 19:38:49 +0700

    update

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -116,7 +116,10 @@ class Home extends CI_Controller {
 		$data['sub_kategori'] = $kategori[1];
 
 		$data['model'] = $this->M_bankdata;
-		$data['dataProvider'] = $this->M_bankdata->select_all();
+		$data['dataProvider'] = $this->M_bankdata->select_all([
+			'kategori' => $kategori[0],
+			'sub_kategori' => $kategori[1]
+		]);
 		$data['page_type'] = 'frontend';
 		
 		$this->template->views('page/kasia/list', $data);		
