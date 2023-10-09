c083172763170a61d54165ee61693701d17bf3d7
Agus Puryanto <aguspuryanto@gmail.com>
2023-10-09 20:41:43 +0700

    update

--- application/views/page/user/setting.php
+++ application/views/page/user/setting.php
@@ -52,8 +52,7 @@
     </div>
 </div>
 
-<?php echo $this->session->userdata('role_id'); ?>
-<div class="row" style="<?=($this->session->userdata('role_id')=='1') ? 'display:block' : 'display:none';?>">
+<div class="row" style="<?=($this->session->userdata('role_id')==1) ? 'display:block' : 'display:none';?>">
     <div class="col-md-12">    
         <div class="card mb-4 mb-xl-0">
             <div class="card-header">
