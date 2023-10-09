744ec0da8be8b16966c7267d315b356009f386b6
Agus Puryanto <aguspuryanto@gmail.com>
2023-10-10 04:20:20 +0700

    update

--- application/views/page/user/_list_user.php
+++ application/views/page/user/_list_user.php
@@ -7,14 +7,14 @@
             <?php
             if($dataProvider) :
                 foreach($dataProvider as $row) {
-                    $imgPct = ($dataUser->picture_img) ? base_url() . 'uploads/' . $dataUser->picture_img : base_url() . 'assets/img/undraw_profile_1.svg';
+                    $imgPct = ($row->picture_img) ? base_url() . 'uploads/' . $row->picture_img : base_url() . 'assets/img/undraw_profile_1.svg';
                     echo '<tr>
                         <td>'.$row->id.'</td>
                         <td>'.$row->username.'</td>
                         <td>'.$row->nama.'</td>
                         <td>'.$row->email.'</td>
                         <td>'.$row->nohape.'</td>
-                        <td><img class="img-account-profile mb-2" src="' . $imgPct . '" alt=""></td>
+                        <td><img class="img-account-profile mb-2" src="' . $imgPct . '" width="70" alt=""></td>
                         <td style="min-width:115px">
                             <div class="btn-group" role="group">
                                 <button type="button" data-id="'.$row->id.'" class="btn btn-secondary btnEdit" data-toggle="modal" data-target="#myModal">Edit</button>
