baf6fb35929bc229ecabc6008dd61e03378c3d0c
Agus Puryanto <aguspuryanto@gmail.com>
2023-10-12 01:46:13 +0700

    update

---
+++ application/models/M_perkara.php
@@ -0,0 +1,59 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class M_partai extends CI_Model {
+    public $table_name = "epak_partai";
+
+    public function rules()
+    {
+        return [
+            ['field' => 'perkara', 'label' => 'Perkara', 'rules' => 'required'],
+            ['field' => 'tipe', 'label' => 'Tipe', 'rules' => 'required']
+        ];
+    }    
+
+    public function save($data) {
+        $this->db->trans_begin();
+        $this->db->insert($this->table_name, $data);
+        $this->db->trans_complete();
+        
+        if ($this->db->trans_status() === FALSE){
+            $this->db->trans_rollback();
+            return 0;
+        } else {
+            $this->db->trans_commit();
+            $insert_id = $this->db->insert_id();
+            return  $insert_id;
+        }
+    }
+
+    public function update($id, $data) {
+        $this->db->where('id', $id);
+        $this->db->update($this->table_name, $data);
+    }
+
+    public function delete($id) {
+        $this->db->where('id', $id);
+        $this->db->delete($this->table_name);
+    }
+
+    public function select_all($options = "") {
+        if($options) {
+            $this->db->where($options);
+        }
+        
+        $data = $this->db->get($this->table_name);
+        return $data->result();
+    }
+
+    public function selectId($id) {
+        $this->db->where('id', $id);
+        $data = $this->db->get($this->table_name);
+        return $data->row();
+    }
+
+    public function total_rows() {
+        $data = $this->db->get($this->table_name);
+        return $data->num_rows();
+    }
+}
