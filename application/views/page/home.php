5e5f1ac5bc7c84ce2ba7011fc8cc7523713cae0b
Agus Puryanto <aguspuryanto@gmail.com>
2023-10-11 23:38:54 +0700

    update

--- application/views/page/home.php
+++ application/views/page/home.php
@@ -28,7 +28,7 @@ $listPeta = getListPeta();
     <nav class="navbar navbar-expand-lg navbar-dark bg-success text-white justify-content-between">
     <div class="container">
         <img src="<?=base_url('assets/');?>img/SI_METAL_BATIN_LOGO.png" alt="logo Kejaksaan" class="mr-3">
-        <a class="navbar-brand text-white" href="<?=site_url('') ?>"><?=@$title; ?> <p class="mb-0"><?=@$desc; ?></p></a>
+        <a class="navbar-brand text-white sr-only" href="<?=site_url('') ?>"><?=@$title; ?> <p class="mb-0"><?=@$desc; ?></p></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <!-- <span class="navbar-toggler-icon"></span> -->
             <span class="navbar-toggler-icon">   
