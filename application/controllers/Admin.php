8c2e44e4762a41c7971f6cd762299bf3a2b432e1
Agus Puryanto <aguspuryanto@gmail.com>
2023-10-12 11:22:33 +0700

    update

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -36,7 +36,8 @@ class Admin extends CI_Controller
 			redirect('dashboard');
 		}
 
-		$this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+		// $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+		$this->form_validation->set_rules('email', 'Email', 'trim|required');
 		$this->form_validation->set_rules('password', 'Password', 'trim|required');
 
 		if ($this->form_validation->run() == false) {
