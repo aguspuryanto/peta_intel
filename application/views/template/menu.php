b30ffb264e186e84bf52ee4086e5fe4346838c22
Agus Puryanto <aguspuryanto@gmail.com>
2023-10-12 13:45:27 +0700

    update

--- application/views/template/menu.php
+++ application/views/template/menu.php
@@ -22,7 +22,7 @@
                 endif;
 
                 $disabledmenu = !array_key_exists($menu['title'], $listMenuDisable);
-                $cssHide = 'd-block' . ($disabledmenu==1) ?? ' disabled';
+                $cssHide = 'd-block ' . ($disabledmenu) ? 'disabled' : 'enabled';
                 if(!in_array($role_id, [3, 12]) && $menu['title'] == 'KASI A') $cssHide = 'd-none';
                 if(!in_array($role_id, [4, 13]) && $menu['title'] == 'KASI B') $cssHide = 'd-none';
                 if(!in_array($role_id, [5, 14]) && $menu['title'] == 'KASI C') $cssHide = 'd-none';
