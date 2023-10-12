4d3b066f770cf629d19fc3c19ecab09d26f22cb3
Agus Puryanto <aguspuryanto@gmail.com>
2023-10-12 14:01:15 +0700

    update

--- application/views/page/user/_account.php
+++ application/views/page/user/_account.php
@@ -2,25 +2,26 @@
         <div class="card mb-4">
             <!-- <div class="card-header">Account Details</div> -->
             <div class="card-body">
+                <?=json_encode($dataUser); ?>
                 <?=form_open('', array('id' => 'formUserAccount', 'role' => 'form'));?>
 
                     <div class="row">
                         <div class="col-md-6">
-                            <?=get_form_input($model, 'username', array('value' => ($dataUser) ?? $dataUser->username)); ?>
+                            <?=get_form_input($model, 'username', array('value' => $dataUser->username)); ?>
                         </div>
                         <div class="col-md-6">
-                            <?=get_form_input($model, 'nama', array('value' => ($dataUser) ?? $dataUser->nama)); ?>
+                            <?=get_form_input($model, 'nama', array('value' => $dataUser->nama)); ?>
                         </div>
                     </div>
 
                     <div class="row">
                         <div class="col-md-6">
-                            <?=get_form_input($model, 'instansi', array('value' => ($dataUser) ?? $dataUser->instansi)); ?>
+                            <?=get_form_input($model, 'instansi', array('value' => $dataUser->instansi)); ?>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label>Role</label>
-                                <?=form_dropdown('role_id', $role, ($dataUser) ?? $dataUser->role_id, array('class' => 'form-control', 'id' => 'input-role_id'));?>
+                                <?=form_dropdown('role_id', $role, $dataUser->role_id, array('class' => 'form-control', 'id' => 'input-role_id'));?>
                                 <div id="error"></div>
                             </div>
                             
@@ -29,14 +30,14 @@
 
                     <div class="row">
                         <div class="col-md-6">
-                            <?=get_form_input($model, 'email', array('value' => ($dataUser) ?? $dataUser->email)); ?>
+                            <?=get_form_input($model, 'email', array('value' => $dataUser->email)); ?>
                         </div>
                         <div class="col-md-6">
-                            <?=get_form_input($model, 'nohape', array('value' => ($dataUser) ?? $dataUser->nohape)); ?>
+                            <?=get_form_input($model, 'nohape', array('value' => $dataUser->nohape)); ?>
                         </div>
                     </div>
 
-                    <?=form_hidden('id', ($dataUser) ?? $dataUser->id); ?>
+                    <?=form_hidden('id', $dataUser->id); ?>
 
                     <button type="submit" class="btn btn-primary" id="formUserSubmit">Buat Akun</button>
 
