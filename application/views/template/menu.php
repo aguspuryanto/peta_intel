74670a6fa69eee6d0fd25e2cc2d8609067b96123
Agus Puryanto <aguspuryanto@gmail.com>
2023-10-12 13:35:04 +0700

    update

--- application/views/template/menu.php
+++ application/views/template/menu.php
@@ -22,7 +22,7 @@
                 endif;
 
                 $cssHide = 'd-block';
-                if(!in_array($role_id, [3, 12]) && $menu['title'] == 'KASI A' && in_array($menu['title'], $listMenuDisable)) $cssHide = 'd-none';
+                if(!in_array($role_id, [3, 12]) && $menu['title'] == 'KASI A' && !in_array($menu['title'], $listMenuDisable)) $cssHide = 'd-none';
                 if(!in_array($role_id, [4, 13]) && $menu['title'] == 'KASI B') $cssHide = 'd-none';
                 if(!in_array($role_id, [5, 14]) && $menu['title'] == 'KASI C') $cssHide = 'd-none';
                 if(!in_array($role_id, [6, 15]) && $menu['title'] == 'KASI D') $cssHide = 'd-none';
